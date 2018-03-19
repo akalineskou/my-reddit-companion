446d9e39486665fe6452b6fcaa517ed4dfde0433
Alex Kalineskou <c.alex2005.mail@gmail.com>
2018-03-19 22:01:04 +0200

    Add console log custom styles by type

--- src/js/debug.js
+++ src/js/debug.js
@@ -1,3 +1,26 @@
 var Debug = {
-    console_logging: false
+    console_logging: false,
+    StyleBytype: function (type) {
+        var style;
+
+        switch (type) {
+            case 'info':
+                style = 'background: green; color: white;';
+                break;
+
+            case 'error':
+                style = 'background: red; color: white;';
+                break;
+
+            case 'debug':
+                style = 'background: black; color: white;';
+                break;
+
+            default:
+                style = 'background: yellow; color: black;';
+                break;
+        }
+
+        return style;
+    }
 };
